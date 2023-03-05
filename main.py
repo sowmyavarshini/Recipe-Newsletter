2e19f8e8ec24a6d4a396b116869f6b5d19316dab
Sowmya <sowmyavarshini.ss@gmail.com>
2023-03-04 23:15:13 -0800

    change sqlite to postgres

--- main.py
+++ main.py
@@ -16,12 +16,13 @@ import os
 
 load_dotenv()
 
-MY_EMAIL = os.getenv("EMAIL")
-PASSWORD = os.getenv("PASSWORD")
+MY_EMAIL = os.environ.get("EMAIL")
+PASSWORD = os.environ.get("PASSWORD")
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///users.db"
+Secret_key = os.getenv("SECRET_KEY")
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", Secret_key)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 schedule = APScheduler()
